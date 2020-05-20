53439f4988a5966b0b90ddad12603df711d9fa0d
prabhjot <prabhhjot.kaur@gmail.com>
2020-05-19 18:46:23 -0700

    folder modified

---
+++ java_1/src/java_1/JavaProject.java
@@ -0,0 +1,15 @@
+package java;
+
+import java.util.Scanner;
+
+public class JavaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter any sentence");
+		String value=input.nextLine();
+
+	}
+
+}
